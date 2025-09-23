b9e048de722907eb85aab4e188dce32161509c06
Sara <sarahraph.j@gmail.com>
2025-09-22 22:18:31 -0400

    Add week03 folder with project files

---
+++ week03/Employee.js
@@ -0,0 +1,27 @@
+//TODO - Create Employee Module here and export to use in index.js
+
+let employees = [
+    {id: 1, firstName: "Pritesh", lastName: "Patel", email: "pritesh@gmail.com", Salary:5000},
+    {id: 2, firstName: "Krish", lastName: "Lee", email: "krish@gmail.com", Salary:4000},
+    {id: 3, firstName: "Racks", lastName: "Jacson", email: "racks@gmail.com", Salary:5500},
+    {id: 4, firstName: "Denial", lastName: "Roast", email: "denial@gmail.com", Salary:9000}
+]
+function getAll(){
+    return employees;
+}
+ 
+function getNamesAscending(){
+    return employees
+    .map(e =>`${e.firstName} ${e.lastName}`)
+    .sort((a, b) => a.localeCompare(b));
+}
+function getTotalSalary() {
+  return employees.reduce((sum, e) => sum + Number(e.Salary || e.salary || 0), 0);
+}
+
+module.exports = {
+  employees,
+  getAll,
+  getNamesAscending,
+  getTotalSalary,
+};
